7b278f29bbdcc9d8d57ce99210c54134aa2e5dcc
hafsafatima333 <fatimahafsa365@gmail.com>
2024-02-16 12:41:49 +0500

    first commit

---
+++ first.js
@@ -0,0 +1,13 @@
+// console.log ("Apna College");
+// console.log ("Hafsa Fatima");
+// console.log ("I Love JavaScript");
+
+fullName = "Tasha sethi";
+age= 24;
+price=49;
+radius=14;
+x=null;
+y=undefined;
+isFollow=true;
+
+console.log (isFollow);
