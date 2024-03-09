9705b7193725c788bb5945f3a4c8f4ed8d9347fd
hafsafatima333 <fatimahafsa365@gmail.com>
2024-03-09 15:52:08 +0500

    learn javascript constructs

--- first.js
+++ first.js
@@ -1,11 +1,533 @@
 
 
-fullName = "Tasha sethi";
-age= 24;
-price=49;
-radius=14;
-x=null;
-y=undefined;
-isFollow=true;
+// // fullName = "Tasha sethi";
+// // age= 24;
+// // price=49;
+// // radius=14;
+// // x=null;
+// // y=undefined;
+// // isFollow=true;
 
-console.log (isFollow);
+// // console.log (isFollow);
+
+// // let x=BigInt ("123");
+// // let y=Symbol ("Hello!")
+// // let x;
+
+// // let x=null;
+
+// // const student= {
+// //     fullName:"Rahul Kumar",
+// //     age:20,
+// //     cgpa:8.2,
+// //     isPass:true,
+// // };
+
+// // student ["age"] = student["age"] + 1;
+// // console.log (student["age"]);
+// // student ["age"]= student ["age"] + 3;
+// // console.log (student["age"]);
+
+
+// // const product= {
+// //     title: "Ball Pen",
+// //     rating: 4 ,
+// //     offer: 5 ,
+// //     price: 270 ,
+// // };
+
+// // console.log (product);
+
+// // const profile= {
+// //     username:"hafsafatima333",
+// //     isFollow:false,
+// //     followers:569,
+// //     following:4,
+// // };
+
+// // console.log  (typeof profile ["username"]);
+// ----------------------------------------------------------------
+// // Airthmetic Operators
+
+// // let a=5;
+// // let b=2;
+// // let c=a+b;
+
+// // console.log ("a+b=",c);
+
+// //let a = 5;
+// //let b = 2;
+
+// //console.log("a =", a, "& b =", b);
+// //console.log("a + b =", a + b);
+// // console.log ("a - b =",a - b);
+// // console.log ("a * b =",a * b);
+// // console.log ("a / b =",a / b);
+// // console.log ("a % b =",a % b);
+// // console.log ("a ** b =",a ** b);
+// -----------------------------------------------------
+// //Airthmetic operators:------>!
+// // Unary Operators
+
+// // let a = 5;
+// // let b = 2;
+
+
+// // console.log("a =", a, "& b =", b);
+
+
+// -----------------------------------------------------------
+// // There are two ways to write unary operators:
+// // let a = 5;
+// // let b = 2;
+
+// // console.log("a =", a, "& b =", b);
+// // ++a;
+// // console.log("a= ", a);
+
+// ------------------------------------------------
+
+// // And the second method should be:
+// // let a = 5;
+// // let b = 2;
+
+// // console.log("a =", a, "& b =", b);
+
+// // console.log("a++ = ", a++);
+// // console.log("a = ", a);
+
+// -----------------------------------------------------------
+
+// // And the third method is this:
+
+// // let a = 5;
+// // let b = 2;
+
+// // console.log("a =", a, "& b =", b);
+// // console.log("--a = ", --a);
+
+
+// ---------------------------------------------------
+// //Assignment operators:---->
+
+// // let a = 5;
+// // let b = 2;
+
+// // a **= 4;// a = a**4
+// // console.log("a=", a);
+
+// ---------------------------------------------------
+// // Comparison operators---->
+// // equal to:
+
+// // let a = 20;
+// // let b = 20;
+
+// // console.log("20 == 20", a == b);
+
+// ---------------------------------------------------
+// //not equal to
+// // let a = 30;
+// // let b = 30;
+
+// //console.log("30 != 30", a != b);
+
+
+// -------------------------------------------------
+// //Strickly comparision operators----->
+// // strickly notequals to ----->
+
+// // let a = 5;//number
+// // let b = "5";//string
+// // console.log("a !== b", a !== b);
+
+
+// -----------------------------------------------
+// //strickly equals to ----->
+
+// // let a = 5;//number
+// // let b = "5";//string
+// // console.log("a === b", a === b);
+
+// --------------------------------------------------------------------------------------------
+// //greater, greater than and equals to , lesser, less than and equals to----->>>>
+// //Greater than:
+// // let a = 5;
+// // let b = 5;
+
+// // console.log("5 > 2", 5 > 2);
+
+// -----------------------------------------------------------
+
+// //Greater than and equals to:
+// // let a = 5;
+// // let b = 5;
+// // console.log("5 >= 5", 5 >= 5);
+
+//  -----------------------------------------------------------
+
+// // lesser  than and equals to:
+// // let a = 5;
+// // let b = 5;
+// // console.log("5 <= 5", 5 <= 5);
+
+// ----------------------------------------------------------
+// //Logical Operators:
+// //logical &&:
+
+// // let a = 6;
+// // let b = 5;
+
+// // let cond1 = a > b;//true
+// // let cond2 = a === 6;//true
+// // console.log("cond1 && cond2 = ", cond1 && cond2);
+
+// -----------------------------------------------------------
+// //LOgical OR || :
+
+// // let a = 6;
+// // let b = 5;
+
+// // console.log(" cond1 || cond2 = ", a < b || a === 6);
+
+// -------------------------------------------------------------
+// //Logical not ! :
+
+// // let a = 6;
+// // let b = 5;
+
+// // console.log("! (6 < 5) = ", !(a < b));
+// --------------------------------------------------------------
+// //Conditional statements:
+// // let age = 16;
+
+// // if (age >= 18) {
+// //     console.log(" you can vote");
+// // }
+
+// // if (age < 18) {
+// //     console.log(" you CANNOT vote");
+// // }
+// --------------------------------------------------------
+// //Conditional statement 2:
+// //For Black mode-------->
+
+
+// // let mode = "dark";
+// // let color;
+
+// // if (mode === "dark"); {
+// //     color = "black";
+// // }
+
+// // if (mode === "light") {
+// //     color = " white ";
+// // }
+
+// // console.log(color);
+
+
+// --------------------------------------------------
+// //For White mode------->
+// // let mode = "light";
+// // let color;
+
+// // if (mode === "dark") {
+// //     color = "black";
+// // }
+
+// // if (mode === "light") {
+// //     color = "white";
+// // }
+
+// // console.log(color);
+
+// ---------------------------------------------------
+// //For else statements----->
+// // let mode = "dark";
+// // let color;
+
+// // if (mode === "dark") {
+// //     color = "black";
+// // } else {
+// //     color = "white";
+// // }
+
+// // console.log(color);
+
+// -------------------------------------------------
+// //One more example----->
+
+// // let age = 16;
+
+// // if (age >= 18) {
+// //     console.log("You can vote");
+// // } else {
+// //     console.log(" You can't vote");
+// // }
+
+// --------------------------------------------------------
+// //Even or odd:------>
+// // let num = 20;
+
+// // if (num % 2 === 0) {
+// //     console.log(num, "is even");
+// // } else {
+// //     console.log(num, "is odd");
+// // }
+
+// ------------------------------------------------------------
+// //odd:------>
+// // let num = 26;
+
+// // if (num % 2 === 0) {
+// //     console.log(num, "is even");
+// // } else {
+// //     console.log(num, "is odd");
+// // }
+
+
+// ----------------------------------------------------------------
+// //Conditional statments:----->
+// // let mode = "pink";
+// // let color;
+
+// // if (mode === "dark") {
+// //     color = "black";
+// // } else if (mode === "green") {
+// //     color = "green";
+// // } else if (mode === "pink") {
+// //     color = "pink";
+// // } else {
+// //     color = "white";
+// // }
+
+// // console.log(color);
+
+
+// //Another example:-------->
+// // let mode = "silver";
+// // let color;
+
+// // if (mode === "dark") {
+// //     color = "black";
+// // } else if (mode === "pink") {
+// //     color = "pink";
+// // } else if (mode === "green") {
+// //     color = "green";
+// // } else {
+// //     color = "yellow";
+// // }
+
+// // console.log(color);
+
+// --------------------------------------------------------------
+// // Ternary Operators:---->
+
+// // let age = "10";
+// // let result = age >= 18 ? "adult" : "not adult";
+// // console.log(result); 
+
+// ----------------------------------
+// //Use of "ALERT"
+// // alert("Hello!");
+
+
+// ------------------------------------------
+// //Use of "PROMPT"
+// // let name = prompt("hello!");
+// // console.log(name);
+
+// ----------------------------------------------------------------------
+// //Practice question1 ---->
+// // let num = prompt("Enter a number");
+
+// // if (num % 5 === 0) {
+// //     console.log(num, "is multiple of 5");
+// // } else {
+// //     console.log(num, "is  NOT multiple of 5");
+// // }
+
+
+// -----------------------------------------------------------------------
+// //Practice question 2----JS program---->>>>
+
+
+// // let score = prompt("enter your score (0-100):");
+// // let grade;
+
+// // if (score >= 90 && score <= 100) {
+// //     grade = "A++";
+// // } else if (score >= 70 && score <= 89) {
+// //     grade = "A";
+// // } else if (score >= 60 && score <= 69) {
+// //     grade = "B";
+// // } else if (score >= 50 && score <= 59) {
+// //     grade = "C";
+// // } else if (score >= 0 && score <= 49) {
+// //     grade = "D";
+// // }
+// // console.log("according to your score , your grade was:", grade);
+
+
+// ------------------------------------------------------------
+// //[Loops in js] ---------->
+// // for (let count = 1; count <= 200; count++) {
+// //     console.log("Hafsa is a cute girl");
+// // }
+
+
+
+// //Another example-------->>>>>>
+
+// // console.log("loop has ended");
+// // for (let num = 1; num <= 450; num++) {
+// //     console.log("I want to gain weight");
+// // }
+// // console.log("Loop has ended!");
+
+// ------------------------------------------------------------
+// [ANOTHER MAJOR EXAMPLE]------->>>
+
+//Calculate sum of 1 to 5 ------------>>>>
+// let sum = 0;
+// for (let i = 1; i <= 5; i++) {
+//     sum = sum + i; // 0 + 1 = 1
+// 1 + 2 = 3
+// 3 + 3 = 6
+// 6 + 4 = 10
+// 10 + 5 = 15
+// }
+// console.log("sum = ", sum);
+// console.log("Loop has been ended");
+
+//Output =
+// Sum = 15
+// ----------------------------------------------------------
+
+// // [ONE MORE EXAMPLE]------>>>>>
+// //  Calculate sum of 1 to 5----->>              
+// // let sum = 0;
+// // let n = 6;
+// // //3 + 1 = 4
+// // // 4 + 2 = 6
+
+// for (let i = 1; i <= n; i++) {
+// //     sum = sum + i;
+// // }
+
+// // console.log("sum = ", sum);
+// // console.log("Loop has been ended");
+
+// ----------------------------------------------------------------------
+
+// //[ANOTHER TYPE OF EXAMPLE]------>>>>
+// // [Loops in js] 
+// // for (let i = 1; i <= 15; i++) {
+// //     console.log("i", i);
+// // }
+
+// let i = 1;
+// while ( i <= 5 ) {
+//     console.log ("i = ", i );
+//     i++;
+// }
+
+// -----------------------------------------------------
+// [USE OF "WHILE" LOOP]:------>
+// let i = 1;
+// while (i <= 5) {
+//     console.log("i =", i);
+//     i++;
+// }
+
+// ------------------------------------------------------
+// ["USE OF DO-WHILE LOOP"]
+// let i = 1;
+// do {
+//     console.log("i=", i);
+//     i++
+// } while (i <= 10);
+
+
+// ANOTHER EXAMPLE
+// let i = 23;
+// do {
+//     console.log("I need anatomy books");
+//     i++;
+// } while (i <= 30);
+
+
+//----------------------------------------------------------------
+// [USE OF FOR-OF LOOP]:--------->>>
+
+// let str = "hafsakhan";
+// for (let i of str) { 
+//this is called iterator.
+//and in iterators CHARACTERS 
+//     console.log("i=", i);
+// }
+
+// ---------------------------------------------------------------
+
+// ANOTHER EXAMPLE-------------------->
+
+// let str = "PECHSCOLLEGE";
+// let size = 0;
+
+// for (let i of str) {
+//     console.log("i=", i);
+//     size++;
+// }
+// console.log("string size", size);
+
+// ###############################################################################################
+
+// [{Here we have i ->iterator but in reality here we use value (val) to make a code better!now use (val) instead of (i).}]
+
+// ###############################################################################################
+// let str = "PECHSCOLLEGE";
+// let size = 0;
+
+// for (let val of str) {
+//     console.log("val=", val);
+//     size++;
+// }
+// console.log("string size", size);
+// -----------------------------------------------------------
+
+// [USE OF FOR-IN LOOP]:
+
+// let student = {
+//     name: "Rahul kumar",
+//     age: 18,
+//     cgpa: 3.5,
+//     isPass: true,
+// };
+
+// for (let key in student) {
+//     console.log("key =", key, "value=", student[key]);
+// }
+
+// ----------------------------------------------------------------
+
+// [PRACTICE SET QUESTION # 1]----->>>
+
+//BY USEing DO-WHILE LOOP:
+// let i = 1;
+// do {
+//     console.log("i=", i);
+//     i++;
+// } while (i <= 100);
+
+
+
+// BY USING FOR LOOP:
+for (num = 0; num <= 100; num++) {
+    if (num % 2 !== 0) {
+        console.log("num=", num);
+    }
+}
+// -------------------------------------------------------------
+// PRACTICE SET QUESTION #2 :
+
